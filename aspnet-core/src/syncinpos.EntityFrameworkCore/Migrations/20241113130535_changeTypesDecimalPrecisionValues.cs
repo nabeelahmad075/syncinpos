41cb0022e25c152c8da5bdf1fa3b5c6b8601166e
VBS-Adnan <muhammad.adnan0313@gmail.com>
2024-11-13 18:07:00 +0500

    decimal types precision values changed

---
+++ aspnet-core/src/syncinpos.EntityFrameworkCore/Migrations/20241113130535_changeTypesDecimalPrecisionValues.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace syncinpos.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeTypesDecimalPrecisionValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TaxPercentOnCreditCard",
+                table: "tblLocations",
+                type: "decimal(10,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TaxPercentOnCredit",
+                table: "tblLocations",
+                type: "decimal(10,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TaxPercentOnCash",
+                table: "tblLocations",
+                type: "decimal(10,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TaxPercentOnBank",
+                table: "tblLocations",
+                type: "decimal(10,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ServiceCharges",
+                table: "tblLocations",
+                type: "decimal(24,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DeliveryCharges",
+                table: "tblLocations",
+                type: "decimal(24,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BankChargesPercent",
+                table: "tblLocations",
+                type: "decimal(24,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,4)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TaxPercentOnCreditCard",
+                table: "tblLocations",
+                type: "decimal(5,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TaxPercentOnCredit",
+                table: "tblLocations",
+                type: "decimal(5,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TaxPercentOnCash",
+                table: "tblLocations",
+                type: "decimal(5,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TaxPercentOnBank",
+                table: "tblLocations",
+                type: "decimal(5,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ServiceCharges",
+                table: "tblLocations",
+                type: "decimal(10,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(24,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DeliveryCharges",
+                table: "tblLocations",
+                type: "decimal(10,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(24,4)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BankChargesPercent",
+                table: "tblLocations",
+                type: "decimal(10,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(24,4)");
+        }
+    }
+}

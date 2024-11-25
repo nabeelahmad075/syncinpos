15f9d2fb75a062bab924422b75fdce6003432189
VBS-Adnan <muhammad.adnan0313@gmail.com>
2024-11-25 17:05:58 +0500

    IsControlAccount Migration

---
+++ aspnet-core/src/syncinpos.EntityFrameworkCore/Migrations/20241125120428_IsControlAccountAddedToSubAccountsAndAccountType.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace syncinpos.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsControlAccountAddedToSubAccountsAndAccountType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsControlAccount",
+                table: "tblSubAccounts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsControlAccount",
+                table: "tblAccountTypes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsControlAccount",
+                table: "tblSubAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "IsControlAccount",
+                table: "tblAccountTypes");
+        }
+    }
+}

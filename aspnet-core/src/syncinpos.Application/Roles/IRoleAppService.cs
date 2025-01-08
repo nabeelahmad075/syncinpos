a3368a41d0f19b7d13511e63a5934f00ca29c463
VBS-Adnan <muhammad.adnan0313@gmail.com>
2025-01-08 13:17:13 +0500

    interface for RoleAppService changes for flatpermissiondto

--- aspnet-core/src/syncinpos.Application/Roles/IRoleAppService.cs
+++ aspnet-core/src/syncinpos.Application/Roles/IRoleAppService.cs
@@ -7,7 +7,7 @@ namespace syncinpos.Roles
 {
     public interface IRoleAppService : IAsyncCrudAppService<RoleDto, int, PagedRoleResultRequestDto, CreateRoleDto, RoleDto>
     {
-        Task<ListResultDto<PermissionDto>> GetAllPermissions();
+        Task<ListResultDto<FlatPermissionDto>> GetAllPermissions();
 
         Task<GetRoleForEditOutput> GetRoleForEdit(EntityDto input);
 
